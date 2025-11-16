db4d4e29c92bdf5a735cdd7f27945d94b5ccdefd
Aysenur Tatli <167679785+aysenurtatli@users.noreply.github.com>
2025-11-16 21:23:20 +0300

    Add page component for project addition with admin access check

---
+++ app/add-project/page.js
@@ -0,0 +1,20 @@
+import { cookies } from "next/headers";
+import ProjectsSample from "./components/ProjectsSample";
+import AddProjectForm from "./components/AddProjectForm";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const cookieStore = await cookies();
+  const isAdmin = cookieStore.get("admin")?.value === "true";
+
+  if (!isAdmin) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="max-w-[1170px] mx-auto min-h-screen md:min-h-[calc(100vh-82.5px-65px)] px-6 py-[40px]">
+      <AddProjectForm />
+      <ProjectsSample />
+    </div>
+  );
+}
