34f745ba3b5de72797142bac696957a8cffde55e
Aysenur Tatli <167679785+aysenurtatli@users.noreply.github.com>
2025-11-16 21:22:52 +0300

    Fix formatting of img tag in Equipments component

--- app/about/components/Equipments.jsx
+++ app/about/components/Equipments.jsx
@@ -6,7 +6,7 @@ const Equipments = () => {
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
         {equipments.map((equipment, index) => (
             <div key={index} className='flex flex-col items-center backdrop-blur-sm border border-white/10 rounded-lg hover:shadow-2xl duration-300 hover:border-indigo-200 p-4'>
-                <img src={equipment.src} alt={equipment.name} className='w-[200px]'/>
+                <img src={equipment.src} alt={equipment.name}  className='w-[200px]'/>
                 <div className='text-indigo-200 text-[15px] md:text-md'>{equipment.name}</div>
             </div>
         ))}
